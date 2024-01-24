e16e21d066abaa90331782243b67f4784bb0dd64
MkhitaryanMartin <1992Martin@mail.ru>
2024-01-24 20:39:06 +0400

    CORS

--- server/index.js
+++ server/index.js
@@ -3,28 +3,27 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const router = require("./routes/todo.js");
 
-
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+// Настройка CORS middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL,
+    origin: "https://todo-5vp3bpzog-mkhitaryanmartin.vercel.app", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
-  }));
-app.use("/todo", router)
+}));
 
+app.use("/todo", router);
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 8010;
-
-const start = async ()=>{
-
+const start = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        app.listen(PORT, ()=> console.log(`SERVER run in ${PORT} port`))
-        
+        await mongoose.connect(process.env.MONGODB_URI);
+        app.listen(PORT, () => console.log(`SERVER run in ${PORT} port`));
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-start()
+start();
