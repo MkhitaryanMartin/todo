395c0d1b4d75fd424e4cd8a7d81ef13c78c3b2cf
MkhitaryanMartin <1992Martin@mail.ru>
2024-01-24 20:45:54 +0400

    CORS

--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ const router = require("./routes/todo.js");
 const app = express();
 app.use(express.json());
 
-
+// Настройка CORS middleware
 app.use(cors({
     origin: "https://todo-sable-three-60.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
+    credentials: true,
+    optionsSuccessStatus: 204 // Добавьте это
 }));
 
+// Добавьте поддержку метода OPTIONS для предварительных запросов
 app.options("*", cors());
 
 app.use("/todo", router);
